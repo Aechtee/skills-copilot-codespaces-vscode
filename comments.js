0a79374cc3066b24920686aca6bcde25e813b79a
Aechtee <139795761+Aechtee@users.noreply.github.com>
2023-07-17 18:04:53 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,1 +1,16 @@
-// Create Web Server
+// Create web server
+
+// Import modules
+var express = require('express');
+var router = express.Router();
+var Comment = require('../models/comment');
+var User = require('../models/user');
+
+// Create comment
+router.post('/create', function(req, res){
+	var comment = new Comment({
+		content: req.body.content,});
+    comment.save(function(err, comment){ // Save comment
+        if(err) return res.status(500).json({error: err});
+        res.json({message: 'Comment created', comment: comment});
+    })});
