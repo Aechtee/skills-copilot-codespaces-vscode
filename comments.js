6e9a6325f60dcbe8f9d9bbf08497be237e22d4f5
Aechtee <139795761+Aechtee@users.noreply.github.com>
2023-07-17 17:51:32 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,89 +1,58 @@
-// Create Web server 
+// Create Web Server
 // Run: node comments.js
-// Node.js - ExpressJS Framework - Comments Application
+// Access: http://localhost:3000/comments
+// Comments: http://localhost:3000/comments?name=John&comment=Hello
+// Comments: http://localhost:3000/comments?name=John&comment=Hello&delete=1
 
-// Import express module
-var express = require('express'); 
-// Import body-parser
-var bodyParser = require('body-parser');
-// Import morgan
-var morgan = require('morgan');
-// Import mongoose
-var mongoose = require('mongoose');
-// Import comment model
-var Comment = require('./models/comment');
-// Import dish model
-var Dish = require('./models/dish');
-// Import promotion model
-var Promotion = require('./models/promotion');
-// Import leader model
-var Leader = require('./models/leader');
-// Import config file
-var config = require('./config');
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var qs = require('querystring');
 
-// Set up express app
-var app = express();
-
-// Connect to MongoDB database
-// 'mongodb://localhost:27017/conFusion'
-mongoose.connect(config.database);
-
-// Set up body-parser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// Set up morgan
-app.use(morgan('dev'));
-
-// Set up port
-var port = process.env.PORT || 3000;
-
-// Set up router
-var router = express.Router();
-
-// Middleware
-router.use(function(req, res, next) {
-    console.log('Something is happening');
-    next();
-});
-
-// Test route
-router.get('/', function(req, res) {
-    res.json({ message: 'Welcome to our API!' });   
-});
-
-// Routes that end in /comments
-router.route('/comments')
-    // Create a comment
-    .post(function(req, res) {
-        var comment = new Comment();
-        comment.rating = req.body.rating;
-        comment.comment = req.body.comment;
-        comment.author = req.body.author;
-        comment.dish = req.body.dish;
-
-        comment.save(function(err) {
-            if (err)
-                res.send(err);
-            res.json({ message: 'Comment created!' });
-        });
-    })
-    // Get all comments
-    .get(function(req, res) {
-        Comment.find(function(err, comments) {
-            if (err)
-                res.send(err);
-            res.json(comments);
-        });
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  console.log(filename);
+  if (filename == './comments') {
+    var qdata = q.query;
+    console.log(qdata);
+    if (qdata.name && qdata.comment) {
+      fs.appendFile('comments.txt', qdata.name + ' ' + qdata.comment + '\n', function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
+    }
+    if (qdata.delete) {
+      fs.writeFile('comments.txt', '', function (err) {
+        if (err) throw err;
+        console.log('Deleted!');
+      });
+    }
+    fs.readFile('comments.txt', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write('<form action="comments">');
+      res.write('Name: <input type="text" name="name"><br>');
+      res.write('Comment: <input type="text" name="comment"><br>');
+      res.write('<input type="submit">');
+      res.write('</form>');
+      res.write('<br>');
+      res.write('<a href="comments?delete=1">Delete</a>');
+      res.write('<br>');
+      res.write('<br>');
+      res.write('Comments:');
+      res.write('<br>');
+      res.write(data);
+      res.end();
     });
-
-// Routes that end in /comments/:comment_id
-router.route('/comments/:comment_id')
-    // Get the comment with that id
-    .get(function(req, res) {
-        Comment.findById(req.params.comment_id, function(err, comment) {
-            if (err)
-                res.send(err);
-            res.json(comment);
-        });
-    })
+  } else {
+    fs.readFile(filename, function(err, data) {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found");
+      }
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+  }
+}).listen(3000);
