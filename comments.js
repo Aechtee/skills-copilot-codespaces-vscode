1841eb7621a9f3616b333cfae69f2266c943478f
Aechtee <139795761+Aechtee@users.noreply.github.com>
2023-07-17 17:58:26 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,49 +1,29 @@
 // Create Web Server
-// Run: node comments.js
-// View at: http://localhost:8080
-
-var http = require('http');
-var url = require('url');
-var util = require('util');
-var querystring = require('querystring');
-
-var comments = [];
-
-http.createServer(function(req, res) {
-    // Parse URL
-    var pathname = url.parse(req.url).pathname;
-
-    // If request is for favicon.ico, return 404
-    if (pathname === '/favicon.ico') {
-        return;
-    }
-
-    // If request is to add comment, get query string
-    var query = url.parse(req.url).query;
-
-    // If query string exists, add comment
-    if (query) {
-        var comment = querystring.parse(query)['comment'];
-        comments.push(comment);
-    }
-
-    // Write HTML
-    res.writeHead(200, {'Content-Type': 'text/html'});
-
-    res.write('<html><head><title>Comments</title></head><body>');
-    res.write('<h1>Comments</h1>');
-
-    // Write comments
-    for (var i = 0; i < comments.length; i++) {
-        res.write('<p>' + comments[i] + '</p>');
-    }
-
-    // Write form
-    res.write('<form action="/" method="get">');
-    res.write('<input type="text" name="comment">');
-    res.write('<input type="submit" value="Submit">');
-    res.write('</form>');
-
-    res.write('</body></html>');
-    res.end();
-}).listen(8080);
+//    run: node comments.js
+//    run: npm install express
+//    run: npm install body-parser
+//    run: npm install method-override
+//    run: npm install express-sanitizer
+//    run: npm install mongoose
+//    run: npm install ejs
+//    run: npm install --save-dev nodemon
+//    run: npm install moment --save
+//    run: npm install connect-flash --save
+//    run: npm install passport --save
+//    run: npm install passport-local --save
+//    run: npm install passport-local-mongoose --save
+//    run: npm install express-session --save
+//    run: npm install cookie-parser --save
+//    run: npm install connect-flash --save
+//    run: npm install multer --save
+//    run: npm install cloudinary --save
+//    run: npm install cloudinary-core --save
+//    run: npm install dotenv --save
+//    run: npm install -g nodemon
+//    run: npm install -g eslint
+//    run: npm install eslint-plugin-import --save-dev
+//    run: npm install eslint-config-airbnb-base --save-dev
+//    run: npm install eslint-plugin-node --save-dev
+//    run: npm install eslint-plugin-promise --save-dev
+//    run: npm install eslint-plugin-standard --save-dev
+//
